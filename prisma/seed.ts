1df0ce22834fe35668dd97c469e6705a028525d5
rehfeld13 <rafaelrehfeld97@gmail.com>
2025-09-20 14:14:10 -0300

    seed, env example and cors

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const passwordHash = await bcrypt.hash('senha123', 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email: 'teste@exemplo.com',
+      password: passwordHash,
+    },
+  });
+
+  console.log('Usuário criado:', user);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
